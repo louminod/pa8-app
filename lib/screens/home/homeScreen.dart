e672687f9ef7cab54744fb3f67c06d8ab4518d0b
louis-marie <louismarie.merminod@gmail.com>
2021-03-11 21:22:48 +0100

    Update homeScreen

--- lib/screens/home/homeScreen.dart
+++ lib/screens/home/homeScreen.dart
@@ -10,6 +10,23 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(actions: <Widget>[
+        IconButton(
+          icon: const Icon(Icons.person_add),
+          tooltip: 'Se connecter',
+          onPressed: () {
+
+          },
+        ),
+      ]),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Navigator.pushNamed(context, Routes.analyse);
+        },
+        child: Icon(Icons.camera_alt),
+        backgroundColor: Colors.blue,
+      ),
+    );
   }
 }
